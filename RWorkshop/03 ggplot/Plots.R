80588bbf2f501e4de000503fef4fb976692563d0
Phil Cannata <phil.cannata@oracle.com>
2015-01-15 15:20:01 -0600

    Changes to get knit html to work - took out jdbc stuff

--- RWorkshop/03 ggplot/Plots.R
+++ RWorkshop/03 ggplot/Plots.R
@@ -1,19 +1,6 @@
-"library("ggplot2")
+# "library("ggplot2")
 options(java.parameters="-Xmx2g")
-library(rJava)
-library(RJDBC)
 
-jdbcDriver <- JDBC(driverClass="oracle.jdbc.OracleDriver", classPath="/Library/Java/JavaVirtualMachines/jdk1.7.0_51.jdk/Contents/Home/ojdbc6.jar")
-
-# In the following, use your username and password instead of "CS347_prof", "orcl_prof" once you have an Oracle account
-possibleError <- tryCatch(
-  jdbcConnection <- dbConnect(jdbcDriver, "jdbc:oracle:thin:@128.83.138.158:1521:orcl", "C##cs347_prof", "orcl_prof"),
-  error=function(e) e
-)
-if(!inherits(possibleError, "error")){
-  diamonds <- dbGetQuery(jdbcConnection, "select * from diamonds d, diam_sale s, diam_retailer r where d.\"diamond_id\" = s.diamond_id and s.retailer_id = r.retailer_id")
-  dbDisconnect(jdbcConnection)
-}
 head(diamonds)
 
 ggplot(data = diamonds) + geom_histogram(aes(x = carat))
