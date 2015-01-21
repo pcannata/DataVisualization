c1ffe8726ee2ecd7c10ad54ea4e96f39288196db
Phil Cannata <phil.cannata@oracle.com>
2015-01-20 20:54:33 -0600

    Fixed library path problem

--- RWorkshop/02 R Dataframes/Dataframes.R
+++ RWorkshop/02 R Dataframes/Dataframes.R
@@ -7,5 +7,5 @@ head(subset(diamonds, select = c(carat, cut)))
 "Selecting a subset of rows from a dataframe:"
 head(subset(diamonds, cut == "Ideal" & price > 5000))
 "Find average price group by color (plyr package is needed)"
-library("plyr", lib.loc="/Library/Frameworks/R.framework/Versions/3.0/Resources/library")
+library("plyr")
 ddply(subset(diamonds, cut == "Ideal" & price > 5000),~color, summarise, o=mean(price, na.rm = TRUE))
