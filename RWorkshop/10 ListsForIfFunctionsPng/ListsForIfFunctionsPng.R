307482f6c21afaa0a99bd32df06cf9d21d38ecf3
Phil Cannata <phil.cannata@oracle.com>
2015-02-19 18:20:52 -0600

    Pushing examples from class

--- RWorkshop/10 ListsForIfFunctionsPng/ListsForIfFunctionsPng.R
+++ RWorkshop/10 ListsForIfFunctionsPng/ListsForIfFunctionsPng.R
@@ -1,8 +1,7 @@
-# for (i in c("cut", "color", "clarity")) {
-#    gdf <- group_by(df, i) %>% summarize(n=n()) 
-#    head(gdf)
-#    ggplot(gdf, aes(x=i, y=n)) + geom_line()
-# }
+# for (i in c("cut", "color", "clarity")) {group_by(diamonds, i) %>% summarize(n=n()) %>% tbl_df}
+
+q='Good'
+r <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from diamonds where \\\"cut\\\" = \\\'"q"\\\'"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDB1.usuniversi01134.oraclecloud.internal',USER='DV_Diamonds',PASS='orcl',MODE='native_mode',MODEL='model',returnDimensions = 'False',returnFor = 'JSON', q=q),verbose = TRUE)))
 
 require(tidyr)
 require(dplyr)
@@ -32,6 +31,32 @@ pushViewport(viewport(layout = grid.layout(2, 12)))
 
 print(l[[1]], vp = viewport(layout.pos.row = 1, layout.pos.col = 1:6))
 print(l[[2]], vp = viewport(layout.pos.row = 1, layout.pos.col = 7:12))
-print(l[[3]], vp = viewport(layout.pos.row = 2, layout.pos.col = 1:6))
+print(l[[3]], vp = viewport(layout.pos.row = 2, layout.pos.col = 3:9))
+
+dev.off()
+
+
+myplot1 <- function(df, x) {
+  names(df) <- c("x")
+  ggplot(df, aes(x=x)) + geom_histogram()
+}
+l <- list()
+for (i in names(diamonds)) {   
+  # For details on [[...]] below, see http://stackoverflow.com/questions/1169456/in-r-what-is-the-difference-between-the-and-notations-for-accessing-the
+  if (i %in% categoricals[[2]]) {
+    r <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select \\\""i"\\\" from diamonds where \\\""i"\\\" is not null "'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDB1.usuniversi01134.oraclecloud.internal',USER='DV_Diamonds',PASS='orcl',MODE='native_mode',MODEL='model',returnDimensions = 'False',returnFor = 'JSON', i=i),verbose = TRUE)))
+    p <- myplot1(r,i)
+    print(p) 
+    l[[i]] <- p
+  }
+}
+
+png("/Users/pcannata/Mine/UT/GitRepositories/DataVisualization/RWorkshop/00 Doc/categoricals.png", width = 25, height = 25, units = "in", res = 72)
+grid.newpage()
+pushViewport(viewport(layout = grid.layout(2, 12)))   
+
+print(l[[1]], vp = viewport(layout.pos.row = 1, layout.pos.col = 1:6))
+print(l[[2]], vp = viewport(layout.pos.row = 1, layout.pos.col = 7:12))
+print(l[[3]], vp = viewport(layout.pos.row = 2, layout.pos.col = 3:9))
 
 dev.off()
