645244ebd371be670b8befae921edf29b32628f5
Phil Cannata <phil.cannata@oracle.com>
2015-04-07 20:22:40 -0500

    Updated Crosstabs.R

--- RWorkshop/16 Crosstabs/Crosstabs.R
+++ RWorkshop/16 Crosstabs/Crosstabs.R
@@ -2,10 +2,11 @@ df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/
 "select
 CASE
 WHEN grouping(job) = 1 THEN \\\'zTotal\\\'
-ELSE job END job, CASE WHEN grouping(dname) = 1 THEN \\\'zTotal\\\'
-ELSE dname END dname, count(*) n
+ELSE job END as job_name,
+CASE WHEN grouping(dname) = 1 THEN \\\'yTotal\\\'
+ELSE dname END as dname, count(*) n
 from emp e join dept d on(e.deptno=d.deptno) group by cube (job, dname)"
-')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDB1.usuniversi01134.oraclecloud.internal', USER='DV_Vehicles', PASS='orcl', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE))); tbl_df(df)
+')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDB1.usuniversi01134.oraclecloud.internal', USER='DV_Scott', PASS='orcl', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE))); tbl_df(df)
 
 spread(df, DNAME, N)
 
@@ -14,9 +15,21 @@ spread(df, DNAME, N)
 
 df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query=
 "SELECT empno, deptno, sal, rank() 
-OVER (PARTITION BY deptno order by sal desc) DEPT_RANK 
+OVER (PARTITION BY deptno order by sal desc) as DEPT_RANK 
 FROM emp order by 2,3 desc"
-')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDB1.usuniversi01134.oraclecloud.internal', USER='DV_Vehicles', PASS='orcl', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE))); tbl_df(df)
+')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDB1.usuniversi01134.oraclecloud.internal', USER='DV_Scott', PASS='orcl', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE))); tbl_df(df)
+
+df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query=
+"SELECT empno, deptno, sal, rank() 
+OVER (order by sal desc) as DEPT_RANK 
+FROM emp "
+')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDB1.usuniversi01134.oraclecloud.internal', USER='DV_Scott', PASS='orcl', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE))); tbl_df(df)
+
+df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query=
+"SELECT * FROM (SELECT empno, deptno, sal, rank() 
+OVER (PARTITION BY deptno order by sal desc) as DEPT_RANK 
+FROM emp) where DEPT_RANK = 1 "
+')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDB1.usuniversi01134.oraclecloud.internal', USER='DV_Scott', PASS='orcl', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE))); tbl_df(df)
 
 df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query=
 "select empno, deptno, sal, last_value(max_sal) 
