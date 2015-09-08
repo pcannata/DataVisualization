9cfac3b13bcf0d38a8b76a7cb60366736a64ae98
Phil Cannata <phil.cannata@oracle.com>
2015-09-08 13:01:13 -0500

    More updates for Sept 8th

--- RWorkshop/02 RESTful Data Access/Access Oracle Database.R
+++ RWorkshop/02 RESTful Data Access/Access Oracle Database.R
@@ -5,38 +5,3 @@ df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?que
 df
 summary(df)
 head(df)
-
-require(extrafont)
-ggplot() + 
-  coord_cartesian() + 
-  scale_x_continuous() +
-  scale_y_continuous() +
-  facet_grid(PCLASS~SURVIVED) +
-  labs(title='Titanic') +
-  labs(x="Age", y=paste("Fare")) +
-  layer(data=df, 
-        mapping=aes(x=as.numeric(as.character(AGE)), y=as.numeric(as.character(FARE)), color=SEX), 
-        stat="identity", 
-        stat_params=list(), 
-        geom="point",
-        geom_params=list(), 
-        #position=position_identity()
-        position=position_jitter(width=0.3, height=0)
-  )
-
-ggplot() + 
-  coord_cartesian() + 
-  scale_x_discrete() +
-  scale_y_continuous() +
-  #facet_grid(PCLASS~SURVIVED) +
-  labs(title='Titanic') +
-  labs(x="SURVIVED", y=paste("FARE")) +
-  layer(data=df, 
-        mapping=aes(x=SEX, y=as.numeric(as.character(FARE)), color=as.character(SURVIVED)), 
-        stat="identity", 
-        stat_params=list(), 
-        geom="point",
-        geom_params=list(), 
-        #position=position_identity()
-        position=position_jitter(width=0.3, height=0)
-  )
