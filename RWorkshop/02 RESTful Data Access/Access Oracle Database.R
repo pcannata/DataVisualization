8a6d350cba39924aba2e67c079ee4fb4c947e491
Phil Cannata <phil.cannata@oracle.com>
2015-09-08 13:07:13 -0500

    Update .Rmd and html files

--- RWorkshop/02 RESTful Data Access/Access Oracle Database.R
+++ RWorkshop/02 RESTful Data Access/Access Oracle Database.R
@@ -2,6 +2,5 @@ require("jsonlite")
 require("RCurl")
 # Change the USER and PASS below to be your UTEid
 df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from titanic where sex is not null"'),httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs347_professor', PASS='orcl_professor', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
-df
 summary(df)
 head(df)
