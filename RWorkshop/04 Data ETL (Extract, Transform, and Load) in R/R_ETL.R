51c258a5b3c5c19236ea253e483ff18e436f136f
Phil Cannata <phil.cannata@oracle.com>
2015-09-15 08:57:39 -0500

    Working on R ETL

---
+++ RWorkshop/04 Data ETL (Extract, Transform, and Load) in R/R_ETL.R
@@ -0,0 +1,47 @@
+setwd("~/Mine/UT/GitRepositories/DataVisualization/RWorkshop/04 Data ETL (Extract, Transform, and Load) in R/CSVs")
+
+file_path <- "April16AdvTblCalc.csv"
+
+df <- read.csv(file_path, stringsAsFactors = FALSE)
+
+# Replace "." (i.e., period) with "_" in the column names.
+names(df) <- gsub("\\.+", "_", names(df))
+
+str(df) # Uncomment this and  run just the lines to here to get column types to use for getting the list of measures.
+
+measures <- c("Customer_ID", "Order_ID", "Postal_Code", "Row_ID", "Discount", "Gross_Profit_Ratio" , "Number_of_Records", "Order_Quantity", "Product_Base_Margin", "Profit", "Sales", "Shipping_Cost", "Unit_Price")
+
+# Get rid of special characters in each column.
+# Google ASCII Table to understand the following:
+for(n in names(df)) {
+  df[n] <- data.frame(lapply(df[n], gsub, pattern="[^ -~]",replacement= ""))
+}
+
+dimensions <- setdiff(names(df), measures)
+for(d in dimensions) {
+  # Get rid of " and ' in dimensions.
+  df[d] <- data.frame(lapply(df[d], gsub, pattern="[\"']",replacement= ""))
+  # Change & to and in dimensions.
+  df[d] <- data.frame(lapply(df[d], gsub, pattern="&",replacement= " and "))
+  # Change : to ; in dimensions.
+  df[d] <- data.frame(lapply(df[d], gsub, pattern=":",replacement= ";"))
+}
+
+# Get rid of all characters in measures except for numbers, the - sign, and period.
+for(m in measures) {
+  df[m] <- data.frame(lapply(df[m], gsub, pattern="[^--.0-9]",replacement= ""))
+}
+
+write.csv(df, paste(gsub(".csv", "", file_path), ".reformatted.csv", sep=""), row.names=FALSE, na = "")
+
+tableName <- gsub(" +", "_", gsub("[^A-z, 0-9, ]", "", gsub(".csv", "", file_path)))
+sql <- paste("CREATE TABLE", tableName, "(\n-- Change table_name to the table name you want.\n")
+for(d in dimensions) {
+   sql <- paste(sql, paste(d, "varchar2(4000),\n"))
+}
+for(m in measures) {
+  if(m != tail(measures, n=1)) sql <- paste(sql, paste(m, "number(38,4),\n"))
+  else sql <- paste(sql, paste(m, "number(38,4)\n"))
+}
+sql <- paste(sql, ");")
+cat(sql)
---
+++ RWorkshop/04 Data ETL (Extract, Transform, and Load) in R/Regular Expression.R
@@ -0,0 +1,34 @@
+#  A regular expression is a special text string for describing a search pattern. You can think of regular expressions as wildcards on steroids. You are probably familiar with wildcard notations such as *.txt to find all text files in a file manager. The regex equivalent is ^.*\.txt$.
+#   The following special characters in a regular expression are often called "metacharacters". 
+#   ?   The preceding item is optional and will be matched at most once.
+#   *   The preceding item will be matched zero or more times.
+#   +   The preceding item will be matched one or more times.
+#   ^   Matches the beginning of a field or line.
+#   $   Matches the end of a field or line.
+#   .   Matches any character
+#   |   or
+#   ()  Grouping
+#   []  Matches one character from the enclosed set of characters.
+#   {n}   The preceding item is matched exactly n times.
+#   {n,}  The preceding item is matched n or more times.
+#   {n,m} The preceding item is matched at least n times, but not more than m times.
+#   \     The following metacharacter losses it's special meaning.
+
+tmp <- head(diamonds, 20)
+tmp
+tmp[1, c('carat', 'cut', 'color')] # Show the first row of tmp.
+grep("Good", tmp$cut, perl=TRUE, value=FALSE)
+
+grep("Good", tmp$cut, perl=TRUE, value=FALSE) %>% tmp[., c('carat', 'cut', 'color')] %>% tbl_df
+grep("G+", tmp$cut, perl=TRUE, value=FALSE) %>% tmp[., c('carat', 'cut', 'color')] %>% tbl_df
+grep("^G+", tmp$cut, perl=TRUE, value=FALSE) %>% tmp[., c('carat', 'cut', 'color')] %>% tbl_df
+grep("^G..d", tmp$cut, perl=TRUE, value=FALSE) %>% tmp[., c('carat', 'cut', 'color')] %>% tbl_df
+grep("^Go*d", tmp$cut, perl=TRUE, value=FALSE) %>% tmp[., c('carat', 'cut', 'color')] %>% tbl_df
+grep("^G.*d", tmp$cut, perl=TRUE, value=FALSE) %>% tmp[., c('carat', 'cut', 'color')] %>% tbl_df
+grep("^Go+d", tmp$cut, perl=TRUE, value=FALSE) %>% tmp[., c('carat', 'cut', 'color')] %>% tbl_df
+grep("^[Gg]o+d", tmp$cut, perl=TRUE, value=FALSE) %>% tmp[., c('carat', 'cut', 'color')] %>% tbl_df
+grep("^[Gg]o+d|Fair", tmp$cut, perl=TRUE, value=FALSE) %>% tmp[., c('carat', 'cut', 'color')] %>% tbl_df
+grep("(Very|Highly) Good", tmp$cut, perl=TRUE, value=FALSE) %>% tmp[., c('carat', 'cut', 'color')] %>% tbl_df
+
+# Changing column values based upon a regular expression
+# d <- with(diamonds, gsub("(Very|Highly) Good", "Great", cut)) %>% diamonds["cut", .]
