c89c49578064eb726ed9d09033e450512311813d
Phil Cannata <phil.cannata@oracle.com>
2015-11-03 16:27:47 -0600

    cleanup

--- RWorkshop/03 Grammar of Graphics with R & ggplot2/Titanic Analysis.R
+++ RWorkshop/03 Grammar of Graphics with R & ggplot2/Titanic Analysis.R
@@ -2,6 +2,7 @@ require("jsonlite")
 require("RCurl")
 # Change the USER and PASS below to be your UTEid
 df <- data.frame(fromJSON(getURL(URLencode('skipper.cs.utexas.edu:5001/rest/native/?query="select * from superstore_sales_orders"'),httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_UTEid', PASS='orcl_UTEid', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+df
 
 summary(df)
 head(df)
