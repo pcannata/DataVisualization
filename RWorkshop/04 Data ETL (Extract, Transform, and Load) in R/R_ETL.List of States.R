c9dadd486f62a9bd48229b200922796f19e8eb36
Phil Cannata <phil.cannata@oracle.com>
2015-11-03 16:27:19 -0600

    Added ETL List of States

---
+++ RWorkshop/04 Data ETL (Extract, Transform, and Load) in R/R_ETL.List of States.R
@@ -0,0 +1,71 @@
+require(tidyr)
+require(dplyr)
+require(ggplot2)
+
+setwd("/Users/pcannata/Mine/UT/cs329e Data Visualization/web-content/Class Notes/_04 Advanced Tableau/Data")
+
+file_path <- "List of States.csv"
+
+df <- read.csv(file_path, stringsAsFactors = FALSE)
+
+# Replace "." (i.e., period) with "_" in the column names.
+names(df) <- gsub("\\.+", "_", names(df))
+
+# Rename Date to 
+#df <- rename(df, ddate=Date)
+
+str(df) # Uncomment this and  run just the lines to here to get column types to use for getting the list of measures.
+
+#measures <- c("Area_Code", "Budget_COGS", "Budget_Margin", "Budget_Profit", "Budget_Sales", "Coffee_Sales" , "COGS", "Inventory", "Margin", "Marketing", "Profit" , "Total_Expenses", "Number_of_Records")
+measures <- NA # Do this if there are no measures.
+
+# Get rid of special characters in each column.
+# Google ASCII Table to understand the following:
+for(n in names(df)) {
+    df[n] <- data.frame(lapply(df[n], gsub, pattern="[^ -~]",replacement= ""))
+}
+
+dimensions <- setdiff(names(df), measures)
+if( length(measures) > 1 || ! is.na(dimensions)) {
+  for(d in dimensions) {
+    # Get rid of " and ' in dimensions.
+    df[d] <- data.frame(lapply(df[d], gsub, pattern="[\"']",replacement= ""))
+    # Change & to and in dimensions.
+    df[d] <- data.frame(lapply(df[d], gsub, pattern="&",replacement= " and "))
+    # Change : to ; in dimensions.
+    df[d] <- data.frame(lapply(df[d], gsub, pattern=":",replacement= ";"))
+  }
+}
+
+library(lubridate)
+# Fix date columns, this needs to be done by hand because | needs to be correct.
+#                                                        \_/
+#df$ddate  <- gsub(" [0-9]+:.*", "", gsub(" UTC", "", mdy(as.character(gsub(" .*", "", df$ddate)),  tz="UTC")))
+
+# The following is an example of dealing with special cases like making state abbreviations be all upper case.
+# df["State"] <- data.frame(lapply(df["State"], toupper))
+
+# Get rid of all characters in measures except for numbers, the - sign, and period.dimensions
+if( length(measures) > 1 || ! is.na(measures)) {
+  for(m in measures) {
+    df[m] <- data.frame(lapply(df[m], gsub, pattern="[^--.0-9]",replacement= ""))
+  }
+}
+
+write.csv(df, paste(gsub(".csv", "", file_path), ".reformatted.csv", sep=""), row.names=FALSE, na = "")
+
+tableName <- gsub(" +", "_", gsub("[^A-z, 0-9, ]", "", gsub(".csv", "", file_path)))
+sql <- paste("CREATE TABLE", tableName, "(\n-- Change table_name to the table name you want.\n")
+if( length(measures) > 1 || ! is.na(dimensions)) {
+  for(d in dimensions) {
+    sql <- paste(sql, paste(d, "varchar2(4000),\n"))
+  }
+}
+if( length(measures) > 1 || ! is.na(measures)) {
+  for(m in measures) {
+    if(m != tail(measures, n=1)) sql <- paste(sql, paste(m, "number(38,4),\n"))
+    else sql <- paste(sql, paste(m, "number(38,4)\n"))
+  }
+}
+sql <- paste(sql, ");")
+cat(sql)
