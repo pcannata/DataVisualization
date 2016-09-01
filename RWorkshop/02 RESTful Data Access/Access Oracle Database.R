3ab61923259d4c25298daa7c6d1719a8a9d78806
Phil Cannata <phil.cannata@oracle.com>
2016-08-31 19:55:37 -0500

    Changes cs327e to cs329e

--- RWorkshop/02 RESTful Data Access/Access Oracle Database.R
+++ RWorkshop/02 RESTful Data Access/Access Oracle Database.R
@@ -1,6 +1,6 @@
 require("jsonlite")
 require("RCurl")
 # Change the USER and PASS below to be your UTEid
-df <- data.frame(fromJSON(getURL(URLencode('oraclerest.cs.utexas.edu:5001/rest/native/?query="select * from emp "'),httpheader=c(DB='jdbc:oracle:thin:@aevum.cs.utexas.edu:1521/f16pdb', USER='cs327e_UTEid', PASS='orcl_uteid', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+df <- data.frame(fromJSON(getURL(URLencode('oraclerest.cs.utexas.edu:5001/rest/native/?query="select * from emp "'),httpheader=c(DB='jdbc:oracle:thin:@aevum.cs.utexas.edu:1521/f16pdb', USER='cs329e_UTEid', PASS='orcl_uteid', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
 summary(df)
 head(df)
