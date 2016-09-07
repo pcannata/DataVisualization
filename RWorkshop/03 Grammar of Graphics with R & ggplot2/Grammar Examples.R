b87a84b5244b4fca9bb61dd83541c9a3d55d8c7c
Phil Cannata <phil.cannata@oracle.com>
2016-09-07 10:40:00 -0500

    Updates from Sept. 6 class

--- RWorkshop/03 Grammar of Graphics with R & ggplot2/Grammar Examples.R
+++ RWorkshop/03 Grammar of Graphics with R & ggplot2/Grammar Examples.R
@@ -16,9 +16,9 @@ labs(title='Diamonds') +
 labs(x="Cut", y="Price") +
 theme_grey() +
 theme(plot.background = element_rect(fill="green", colour=NA)) +
-theme(plot.title=element_text(size=30, face="bold", vjust=1, family="Bauhaus93")) +
+theme(plot.title=element_text(size=10, face="bold", vjust=1, family="Arial")) +
 theme(axis.ticks.y=element_blank(), axis.text.y=element_blank()) +
-theme(axis.text.x=element_text(angle=50, size=20, vjust=0.5)) +
+theme(axis.text.x=element_text(angle=50, size=10, vjust=0.5)) +
 theme(
     axis.title.x=element_text(color="forestgreen", vjust=0.35),
     axis.title.y=element_text(color="cadetblue", vjust=0.35)
--- RWorkshop/03 Grammar of Graphics with R & ggplot2/Titanic Analysis.R
+++ RWorkshop/03 Grammar of Graphics with R & ggplot2/Titanic Analysis.R
@@ -1,14 +1,7 @@
 require("jsonlite")
 require("RCurl")
 # Change the USER and PASS below to be your UTEid
-df <- data.frame(fromJSON(getURL(URLencode('oraclerest.cs.utexas.edu:5001/rest/native/?query="select * from dept"'),httpheader=c(DB='jdbc:oracle:thin:@aevum.cs.utexas.edu:1521/utpdb', USER='cannata', PASS='Cenne!e49', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
-df
-df <- data.frame(fromJSON(getURL(URLencode('ec2-52-34-29-13.us-west-2.compute.amazonaws.com:5011/rest/native/?query="select * from LeaseApplication l join Tenant t on(l.Customer_ID = t.Customer_ID) join LA_Lease x on(l.Lease_ID = x.Lease_ID) join Status s on (l.Lease_id = s.Lease_ID) join Property p on (l.lease_id = p.lease_id) join address a on (p.property_id = a.property_id)"'),httpheader=c(DB='OracleNoSQL', USER='C##cs329e_UTEid', PASS='orcl_UTEid', MODE='rdf_mode', MODEL='A0', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
-df
-df <- data.frame(fromJSON(getURL(URLencode('localhost:5011/rest/native/?query="select * from LeaseApplication l join Tenant t on(l.Customer_ID = t.Customer_ID)"'),httpheader=c(DB='OracleNoSQL', USER='C##cs329e_UTEid', PASS='orcl_UTEid', MODE='rdf_mode', MODEL='A0', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
-df
-save(df, file="/Users/pcannata/Downloads/R2Tableau.RData")
-
+df <- data.frame(fromJSON(getURL(URLencode('oraclerest.cs.utexas.edu:5001/rest/native/?query="select * from titanic where sex is not null"'),httpheader=c(DB='jdbc:oracle:thin:@aevum.cs.utexas.edu:1521/f16pdb', USER='cs329e_UTEid', PASS='orcl_uteid', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
 summary(df)
 head(df)
 
@@ -22,12 +15,13 @@ ggplot() +
   #facet_grid(PCLASS~SURVIVED, labeller=label_both) +
   labs(title='Titanic') +
   labs(x="Age", y=paste("Fare")) +
+  
   layer(data=df, 
-        mapping=aes(x=as.numeric(as.character(AGE)), y=as.numeric(as.character(FARE)), color=SEX), 
-        stat="identity", 
-        stat_params=list(), 
+        mapping=aes(x=as.numeric(as.character(AGE)), y=as.numeric(as.character(FARE)), color=SEX),  
         geom="point",
-        geom_params=list(), 
+        #geom_params=list(), 
+        stat="identity", 
+        #stat_params=list(),
         #position=position_identity()
         position=position_jitter(width=0.3, height=0)
   )
@@ -39,12 +33,13 @@ ggplot() +
   #facet_grid(PCLASS~SURVIVED, labeller=label_both) +
   labs(title='Titanic') +
   labs(x="SURVIVED", y=paste("FARE")) +
+  
   layer(data=df, 
-        mapping=aes(x=SEX, y=as.numeric(as.character(FARE)), color=as.character(SURVIVED)), 
-        stat="identity", 
-        stat_params=list(), 
+        mapping=aes(x=SEX, y=as.numeric(as.character(FARE)), color=as.character(SURVIVED)),  
         geom="point",
-        geom_params=list(), 
+        #geom_params=list(),
+        stat="identity", 
+        #stat_params=list(), 
         #position=position_identity()
         position=position_jitter(width=0.3, height=0)
   )
