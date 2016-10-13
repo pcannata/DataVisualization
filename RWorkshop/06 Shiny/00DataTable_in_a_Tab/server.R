d01c1ed426378bea492a8bf1b9276e4e158e3a7d
Phil Cannata <phil.cannata@oracle.com>
2016-10-13 09:01:06 -0500

    Shortened to sql result set

--- RWorkshop/06 Shiny/00DataTable_in_a_Tab/server.R
+++ RWorkshop/06 Shiny/00DataTable_in_a_Tab/server.R
@@ -11,13 +11,13 @@ require(DT)
 shinyServer(function(input, output) {
 df1 <- eventReactive(input$clicks1, {data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", 'oraclerest.cs.utexas.edu:5001/rest/native/?query=
   "select *
-  from diamonds;"
+  from diamonds
+  where cut = \'Fair\' or cut = \'Premium\';"
 ')), httpheader=c(DB='jdbc:oracle:thin:@aevum.cs.utexas.edu:1521/f16pdb', USER='cs329e_UTEid', PASS='orcl_uteid', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
 })
 
 # Begin code for First Tab:
-      output$table <- renderDataTable({DT::datatable(df1(), rownames = TRUE,
-            extensions = list(Responsive = TRUE, FixedHeader = TRUE)
+output$table <- renderDataTable({DT::datatable(df1(), rownames = TRUE, extensions = list(Responsive = TRUE, FixedHeader = TRUE)
       )
   })
 })
